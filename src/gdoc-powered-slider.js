d6cb4a3b125f960258fb9c238e97c3202056bfe7
Jaeah Lee <jaeahjlee@gmail.com>
2013-01-02 11:27:23 -0800

    refactor to accept json as well as google spreadsheet id

--- src/gdoc-powered-slider.js
+++ src/gdoc-powered-slider.js
@@ -8,16 +8,16 @@
         var back_button_elem;
         var start_slide;
         var slides;
-        var that;
+        var self;
         var pushed_back = false;
 
         var slideshow = {
             container : 'slideshow_container',
-            init : function(slides, options) {
-                that = this;
+            init : function(raw_slides, options) {
+                self = this;
                 if (options) {
                     for ( var option in options ) {
-                        that[option] = options[option];
+                        self[option] = options[option];
                     }
                 }
 
@@ -31,17 +31,17 @@
                     return parseInt(document.location.search.replace('?slide=', ''));
                 })()
 
-                if (typeof(slides) === 'string') {
+                if (typeof(raw_slides) === 'string') {
                     //is a google spreadsheet
-                    that.make_slides_from_google_spreadsheet(slides);
-                    return that;
+                    self.make_slides_from_google_spreadsheet(raw_slides);
+                    return self;
                 }
-                that.slides = slides;
+                self.slides = self.make_html_from_slide_data(raw_slides);		
                     
-                that.create_cover();
+                self.create_cover();
 
-                for ( var i = 0; i < that.slides.length; i++ ) {
-                    that.append_slide(slides[i]);
+                for ( var i = 0; i < self.slides.length; i++ ) {
+                    self.append_slide(self.slides[i]);
                 }
 
                 jQuery(window).bind("popstate", function(e) {
@@ -60,11 +60,11 @@
                     if (e.originalEvent.state) {
                         pushed_back = true;
                     }
-                    that.swipe.slide(slide);
+                    self.swipe.slide(slide);
                 });
                 pushed_back = false;
 
-                that.swipe = new Swipe(document.getElementById(that.container),
+                self.swipe = new Swipe(document.getElementById(self.container),
                         {
                             startSlide: parseInt(start_slide),
                             speed: 500,
@@ -72,7 +72,7 @@
                                 cover.find('.control_container .disabled').removeClass('disabled');
                                 if (index === 0) {
                                     cover.find('.control_container #slideshow_back_button').addClass('disabled');
-                                } else if (index === that.slides.length) {
+                                } else if (index === self.slides.length) {
                                     cover.find('.control_container #slideshow_next_button').addClass('disabled');
                                 }
                                 cover.find('.selected').removeClass('selected');
@@ -85,33 +85,33 @@
                         }
                 );
 
-                that.control_container = that.create_controls();
-                cover.append(that.control_container)
-                if (that.swipe.index === 0) {
+                self.control_container = self.create_controls();
+                cover.append(self.control_container)
+                if (self.swipe.index === 0) {
                     cover.find('.control_container #slideshow_back_button').addClass('disabled');
-                } else if (that.swipe.index === (that.slides.length - 1)) {
+                } else if (self.swipe.index === (self.slides.length - 1)) {
                     cover.find('.control_container #slideshow_next_button').addClass('disabled');
                 }
 
                 jQuery(document).bind('resize', function() {
-                    that.control_container.remove();
-                    that.pagination.remove();
-                    that.cover.html('');
-                    that.init();
+                    self.control_container.remove();
+                    self.pagination.remove();
+                    self.cover.html('');
+                    self.init();
                 });
                 cover.mouseout(function(e) {
-                    jQuery(document).unbind('keydown', that.keydown_event);
+                    jQuery(document).unbind('keydown', self.keydown_event);
                 });
                 cover.mouseover(function() {
-                    jQuery(document).keydown(that.keydown_event);
+                    jQuery(document).keydown(self.keydown_event);
                 });
-                return that;
+                return self;
             },
             keydown_event : function(e) {
                     if (e.keyCode === 37) {
-                        that.back()
+                        self.back()
                     } else if (e.keyCode === 39) {
-                        that.next()
+                        self.next()
                     }
                     return false;
             },
@@ -119,18 +119,23 @@
                 Tabletop.init({ 
                     key: spreadsheet_id,
                     callback: function(data) {
-                        var slides_data = that.make_slides_data_from_spreadsheet_data(data);
-                        that.init(slides_data, options);
+                        var slides_data = self.make_slides_data_from_spreadsheet_data(data);
+                        self.init(slides_data, options);
                     },
                     simpleSheet: true
                 });
             },
-            make_slides_data_from_spreadsheet_data: function(data) {
-                var slides = [];
+
+			make_slides_data_from_spreadsheet_data: function(data) {
+				return data;
+			},
+
+            make_html_from_slide_data: function(data) {
+				var slides = [];
                 for (var i = 0; i < data.length; i++) {
                     var row = data[i];
                     var slide = {}; 
-                    slide.html = that.build_slide_html_from_row(row);
+                    slide.html = self.build_slide_html_from_row(row);
                     slides[i] = slide;
                 }
                 return slides;
@@ -156,27 +161,27 @@
                     + '</li>';
             },
             create_cover : function() {
-                cover = $('#' + that.container);
+                cover = $('#' + self.container);
                 container_elem = jQuery('<ul></ul>');
                 cover.append(container_elem);
                 container_elem.addClass('slideshow_container');
                 container_elem.css('padding', '0px');
-                that.set_slider_width();
+                self.set_slider_width();
             },
             create_pagination: function() {
                 var pagination_container = jQuery('<ul class="pagination_container" style="display: inline; list-style-type: none;"></ul>');
-                for ( var i = 0; i < that.slides.length; i++ ) {
+                for ( var i = 0; i < self.slides.length; i++ ) {
                     item = jQuery('<li class="pagination_' + i
                             + (i === parseInt(start_slide) ? ' selected' : '') 
                             + '">' + (i + 1) +'</li>');
                     (function(item, i) {
                         item.bind('click', function() {
-                            that.swipe.slide(i);
+                            self.swipe.slide(i);
                         });
                     })(item, i);
                     pagination_container.append(item);
                 }
-                pagination_container.after(jQuery('<span> of ' + that.slides.length + '</span>'));
+                pagination_container.after(jQuery('<span> of ' + self.slides.length + '</span>'));
                 return pagination_container;
             },
             set_slider_width : function() {
@@ -191,10 +196,10 @@
             },
             create_controls : function() {
                 var control_container = jQuery('<div class="control_container"></div>');
-                control_container.append(that.create_back_button_elem());
-                that.pagination = that.create_pagination();
-                control_container.append(that.pagination);
-                control_container.append(that.create_next_button_elem());
+                control_container.append(self.create_back_button_elem());
+                self.pagination = self.create_pagination();
+                control_container.append(self.pagination);
+                control_container.append(self.create_next_button_elem());
 
                 return control_container;
             },
@@ -202,7 +207,7 @@
                 back_button_elem = $('<a id="slideshow_back_button"' + 
                     ' value="back">Back</a>'
                 );
-                back_button_elem.click(function(){that.back();});
+                back_button_elem.click(function(){self.back();});
                 back_button_elem.addClass('slideshow_top_controls');
                 return back_button_elem;
             },
@@ -210,23 +215,23 @@
                 next_button_elem = $('<a id="slideshow_next_button"' + 
                     ' value="next">Next</a>'
                 );
-                next_button_elem.click(function(){that.next();});
+                next_button_elem.click(function(){self.next();});
                 next_button_elem.addClass('slideshow_top_controls');
                 return next_button_elem;
             },
             append_slide : function(slide) {
-                slide.element = jQuery(slide.html)
+                slide.element = jQuery(slide.html);
                 container_elem.append(slide.element);
             },
             display_slide : function(slide) {
-                that.swipe(slide);
+                self.swipe(slide);
             },
             back : function() {
-               that.swipe.prev();
+               self.swipe.prev();
             },
             next : function() {
-               if ( that.swipe.index !== (that.slides.length - 1) ) { 
-                   that.swipe.next();
+               if ( self.swipe.index !== (self.slides.length - 1) ) { 
+                   self.swipe.next();
                }
             }
         };
